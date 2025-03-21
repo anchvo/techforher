aac8d0047a5befda58ac98d0b827f8997345eec5
NontyD <nonty@gmail.com>
2025-03-21 13:31:10 +0000

    Add user management views and templates for listing, creating, updating, and deleting users

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -1,6 +1,18 @@
 from . import views
 from django.urls import path
+from .views import (
+    UserListView,
+    UserDetailView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+)
 
 urlpatterns = [
-        path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('users/create/', UserCreateView.as_view(), name='user-create'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,9 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import User
+
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'homeapp/index.html')
+
+# List all users
+class UserListView(ListView):
+    model = User
+    template_name = 'homeapp/user_list.html'
+    context_object_name = 'users'
+
+# Display user details
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'homeapp/user_detail.html'
+    context_object_name = 'user'
+
+# Create a new user
+class UserCreateView(CreateView):
+    model = User
+    fields = ['username', 'email', 'role', 'is_verified']
+    template_name = 'homeapp/user_form.html'
+    success_url = reverse_lazy('user-list')
+
+# Update an existing user
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['username', 'email', 'role', 'is_verified']
+    template_name = 'homeapp/user_form.html'
+    success_url = reverse_lazy('user-list')
+
+# Delete a user
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'homeapp/user_confirm_delete.html'
+    success_url = reverse_lazy('user-list')
