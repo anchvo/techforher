af1a86c52a2f9432f62df990e241e1f3e11ff3b6
Jo <joanne.c.wright@googlemail.com>
2025-03-21 12:13:47 +0000

    set up html page templates

--- homeapp/views.py
+++ homeapp/views.py
@@ -7,3 +7,15 @@ from django.views import generic
 
 def index(request):
     return render(request, 'homeapp/index.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def forum(request):
+    return render(request, 'forum.html')
+
+def connect(request):
+    return render(request, 'connect.html')
