8cdced326dde22be61a705c6fc62fe42657b523e
Jo <joanne.c.wright@googlemail.com>
2025-03-21 16:42:55 +0000

    add form for forumpost

--- homeapp/views.py
+++ homeapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from .models import ForumPost
+from .forms import ForumPostForm
 # Create your views here.
 
 
@@ -15,8 +16,17 @@ def about(request):
     return render(request, 'homeapp/about.html')
 
 def forum(request):
+    if request.method == 'POST':
+        form = ForumPostForm(request.POST)
+        if form.is_valid():
+            forum_post = form.save(commit=False)
+            forum_post.user = request.user
+            forum_post.save()
+            return redirect('forum')
+    else:
+        form = ForumPostForm()
     posts = ForumPost.objects.all()
-    return render(request, 'homeapp/forum.html', {'posts': posts})
+    return render(request, 'homeapp/forum.html', {'posts': posts, 'form': form})
 
 def connect(request):
     return render(request, 'homeapp/connections.html')
