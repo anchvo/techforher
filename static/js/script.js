5368c14d234f6e11bd0c5d583593396c93f8d5c3
anchvo <ac.voretzsch@gmx.de>
2025-03-24 00:19:38 +0100

    Add navbar css styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,17 @@
+// Get elements
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+// Toggle the menu visibility when the burger icon is clicked
+menuToggle.addEventListener('click', function() {
+    menu.classList.toggle('show'); // Toggle 'show' class to show/hide the menu
+});
+
+/* Old JS Code
 const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+*/
